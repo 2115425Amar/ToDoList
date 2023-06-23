ef9b27ef0c3d43fd3074886eac789a613cde4d7c
Amar <amar2115425@akgec.ac.in>
2023-06-23 23:36:04 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -1,100 +1,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose =require("mongoose");
-
-//const router =express.Router;
-const PORT = process.env.PORT || 3600
-//const ejs = require('ejs');
 const app = express();
+app.set("view engine", "ejs");
 
-var items=[];
-
-app.set('view engine','ejs')
+const path = require("path");
+ app.set('views', path.join(__dirname, 'views'));
 
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static("public"));
 
-//mongodb://0.0.0.0:27017/todolistDB
-mongoose.connect('mongodb+srv://amar1234:Vaibhav1234@cluster0.sq2qahp.mongodb.net/todolistDB', {useNewUrlParser: true,
-useUnifiedTopology:true});
-
-var itemsSchema = new mongoose.Schema({
-    name: String
-  });
-  
-  var Item =mongoose.model('Item._id',itemsSchema);
-
-  const item1=new Item({
-    name:"this is item1"
-  });
-
-  const item2=new Item({
-    name:"item2"
-  });
-
-  const item3=new Item({
-    name:"item3 "
-  });
-  
-  const defaultItems=[item1,item2,item3];
+var items=["Buy Food","Cook Food","Eat Food"];
 
 app.get("/", function(req, res) {
-
-    var today = new Date();
-
-    var options={
-        weekday:"long",
-        day:"numeric",
-        month:"long"
-    };
-
-    var day=today.toLocaleDateString("en-US",options);
-
-    Item.find({},function(err,founditems){
-
-      if(founditems.length===0){
-      Item.insertMany(defaultItems,function(err){
-        if(err){
-         console.log(err);
-        }
-        else{
-         console.log("Succesfully saved all the itemsDb");
-        }
-     });
-       }
-       else{
-      res.render("list", {kindofDay: day,newListItem: founditems});
-     }
-  });
+    res.render("list", {newListItems:items});
 });
 
 app.post("/",function(req,res){
-    const itemName=req.body.newItem;
-    const item=new Item({
-      name:itemName
-    });
-    item.save();
+    var item = req.body.newItem;
+    items.push(item);
+    // console.log(item);
+    // res.render("list",{newListItem:item});
     res.redirect("/");
 });
 
-app.post("/delete",function(req,res){
-  //console.log(req.body);
-  const checkedItemId=req.body.checkbox;
-
-  Item.findByIdAndRemove(checkedItemId,function(err){
-    if(!err){
-      console.log("Successfully deleted");
-      res.redirect("/");
-    }
-  })
-});
-
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3600;
-}
-app.listen(port);
-
-app.listen(port, (req, res)=>{
-     console.log("The Server has started successfully"+PORT)
-})
+app.listen(4500, (req, res) => {
+  console.log("The Server has started successfully");
+}); 
